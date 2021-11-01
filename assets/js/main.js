d40b3ffb467cfddbdc7dcb5cb581c891bed7d11e
Daniel Moya <daniel.moya@evertecinc.com>
2021-11-01 17:24:23 -0400

    TOGGLE TITLE  SECTION WHILE SCROLLING

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,10 @@ function scrollActive(){
             sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+           document.getElementById('titleScreen').textContent = document.querySelector('.nav__menu a[href*=' + sectionId + ']').textContent
+           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
@@ -26,3 +27,27 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== TOGGLE TITLE FIRST SECTION WHILE SCROLLING ===============*/
+window.addEventListener('scroll', function() {
+    var titleBar = document.querySelector('#titleScreen'); 
+    
+	var element = document.querySelector('#homeSectionTitle');
+	var position = element.getBoundingClientRect();
+
+    var homeSection = document.querySelector('#homeContainer');
+    var positionHomeSection = homeSection.getBoundingClientRect();
+
+	// checking whether fully visible
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+        titleBar.textContent = '';
+		console.log('Element is fully visible in screen');
+	}
+
+	// checking for partial visibility
+	if(position.top < window.innerHeight && position.bottom >= 0) {
+		console.log('Element is partially visible in screen');
+        //titleBar.textContent = 'Home';
+	}
+
+});
