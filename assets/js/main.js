e8fcd126754b23bf4865ceb568d5c7574cec5e21
Daniel Moya <daniel.moya@evertecinc.com>
2021-11-01 20:35:31 -0400

    Home and About Sections animations completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,13 @@ function scrollActive(){
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
            document.getElementById('titleScreen').textContent = document.querySelector('.nav__menu a[href*=' + sectionId + ']').textContent
            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+           console.log('Id de seccion: '+sectionId+'')
         }else{
            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader(){
@@ -38,15 +38,24 @@ window.addEventListener('scroll', function() {
     var aboutSection = document.querySelector('#about');
     var positionAboutSection = aboutSection.getBoundingClientRect();
 
+    var homeSection = document.querySelector('#home');
+    var positionHomeSection = homeSection.getBoundingClientRect();
+
 	// checking whether bigTitleHomeSection fully visible
 	if(positionBigTitleHomeSection.top >= 0 && positionBigTitleHomeSection.bottom <= window.innerHeight) {
         titleBar.textContent = '';
-		console.log('Element is fully visible in screen');
+		console.log('Big Title on Home Section is fully visible in screen');
+	}
+
+    // checking whether HomeSection fully visible
+	if(positionHomeSection.top >= 0 && positionHomeSection.bottom <= window.innerHeight) {
+        titleBar.textContent = 'Home';
+		console.log('Home Section is fully visible in screen');
 	}
 
 	// checking bigTitleHomeSection for partial visibility
 	if(positionBigTitleHomeSection.top < window.innerHeight && positionBigTitleHomeSection.bottom >= 0) {
-		console.log('Element is partially visible in screen');
+		console.log('Big Title on Home Section is partially visible in screen');
         //titleBar.textContent = 'Home';
 	}
 
